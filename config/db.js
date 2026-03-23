2898702405ed44940dabf7213ef09a5a3461535e
Uday Pratap <97505262+udaysinghrajput12@users.noreply.github.com>
2026-03-24 02:20:34 +0530

    Refactor db.js for improved database connection
    Refactor database connection configuration to use environment variables for host, user, password, database, and port. Enable SSL with rejectUnauthorized set to false.

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+/*require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
@@ -6,6 +6,26 @@ const pool = mysql.createPool({
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'retail_inventory',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+module.exports = pool;*/
+require('dotenv').config();
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+
+    ssl: {
+        rejectUnauthorized: false
+    },
+
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
